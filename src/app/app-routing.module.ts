7f346647d23379c1ef68321eac4564a2e9416963
Emanuel Vinícius Sacoman <emanuelsacoman@gmail.com>
2024-09-28 16:05:04 -0300

    WIP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './view/usuario/entrar/login/login.component';
+import { CadastrarComponent } from './view/usuario/entrar/cadastrar/cadastrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'cadastrar',
+    component: CadastrarComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
